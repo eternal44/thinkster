666ec6bc29a9bd98bbe1caf2b2666ec7960095cb
James Youn <jamesyoun710@gmail.com>
2016-02-03 22:50:12 -0800

    Clean up spacing for Main controller

--- app.js
+++ app.js
@@ -8,26 +8,23 @@ app.factory('posts', [function(){
 }]);
 
 app.controller('MainCtrl', function($scope, posts){
-    $scope.test = 'Hello world!';
+  $scope.addPost = function(){
+    if(!$scope.title || $scope.title === '') return;
+    $scope.posts.push({
+      title: $scope.title,
+      link: $scope.link,
+      upvotes: 0
+    });
+    $scope.title = '';
+    $scope.link = '';
+  };
 
-    $scope.addPost = function(){
-      if(!$scope.title || $scope.title === '') return;
-      $scope.posts.push({
-        title: $scope.title,
-        link: $scope.link,
-        upvotes: 0
-      });
-      $scope.title = '';
-      $scope.link = '';
-    };
+  $scope.incrementUpvotes = function(post){
+    post.upvotes++;
+  };
 
-    $scope.incrementUpvotes = function(post){
-      post.upvotes++;
-    };
-
-    $scope.posts = posts.posts;
-  }
-);
+  $scope.posts = posts.posts;
+});
 
 app.controller('PostCtrl', function($scope, $stateParams, posts){
 });
