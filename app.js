e4abd82044a65432b057ca071caf307bfc1b0620
James Youn <jamesyoun710@gmail.com>
2016-02-03 22:16:13 -0800

    Refactor controller to inject factories

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 var app = angular.module('flapperNews', []);
 
-app.controller('MainCtrl', [ '$scope', function($scope){
-  $scope.test = 'Hello world!';
-  $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
-  ];
-  $scope.addPost = function(){
-    if(!$scope.title || $scope.title === '') return;
-    $scope.posts.push({
-      title: $scope.title,
-      link: $scope.link,
-      upvotes: 0
-    });
-    $scope.title = '';
-    $scope.link = '';
+app.factory('posts', [function(){
+  var o = {
+    posts: []
   };
-  $scope.incrementUpvotes = function(post){
-    post.upvotes++;
-  };
+  return o;
 }]);
+
+app.controller('MainCtrl', function($scope, posts){
+    $scope.test = 'Hello world!';
+
+    $scope.addPost = function(){
+      if(!$scope.title || $scope.title === '') return;
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0
+      });
+      $scope.title = '';
+      $scope.link = '';
+    };
+
+    $scope.incrementUpvotes = function(post){
+      post.upvotes++;
+    };
+
+    console.log(posts);
+    $scope.posts = posts.posts;
+  }
+);
